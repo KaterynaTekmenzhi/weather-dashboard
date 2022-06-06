e4be9e92a590f8f4dcc0c27c4d319659a9cdb7a5
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-06-06 03:41:16 -0700

    starting from scratch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,42 @@
-const repoList = document.querySelector('h3');
+const repoList = document.getElementById('weather-data');
 
 const fetchButton = document.getElementById('fetch-button');
 
-function getApi() {
-    const requestUrl = '';
+const temp = document.getElementById('temp');
+const city = document.getElementById('wind');
+const humid = document.getElementById('humidity');
+const uvIndex = document.getElementById('uv-index');
 
-    fetch(requestUrl)
-    .then(function(res){
-        return res.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            const listItem = document.createElement('li');
-            listItem.innerHTML = data[i].name;
-            repoList.appendChild(listItem);
-            
-        } 
-    });
-}
+// function getApi() {
+//     const requestUrl = 'https://api.github.com/users/katerynatekmenzhi/repos';
 
-fetchButton.addEventListener('click', getApi);
+//     fetch(requestUrl)
+//         .then(function(res){
+//             console.log(res);
+//             return res.json();
+//         })
+//         .then(function (data) {
+//             // looping through the data and inserting the repo name into the h3
+//             console.l**
+//             for (var i = 0; i < data.length; i++) {
+//                 const listItem = document.createElement('li');
+//                 listItem.innerHTML = data[i].name;
+//                 repoList.appendChild(listItem);
+
+//             } 
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+// }
+
+// fetchButton.addEventListener('click', getApi);
+
+// var users = fetch('https://api.github.com/users/katerynatekmenzhi/repos')
+// .then(function(res){
+//     logGitHubData(res);
+// })
+
+// var logGitHubData = function(myUserList) {
+//     console.log(myUserList);
+// }
